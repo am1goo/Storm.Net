76ecb7cffae77bfda2d8a0cd994d57c5d63a564e
p.shestakov <0xShestakov@gmail.com>
2024-04-20 15:22:24 +0300

    added ability to parse multi-line values surrounded by 'quote' marks

--- Ston/StonSerializer.cs
+++ Ston/StonSerializer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Ston
@@ -11,6 +12,7 @@ namespace Ston
     {
         private const char Separator    = ':';
         private const char Equal        = '=';
+        private const char Quote        = '"';
         private const char BraceStart   = '{';
         private const char BraceEnd     = '}';
 
@@ -136,25 +138,26 @@ namespace Ston
 
             var key = line.Substring(0, equalIndex, StonExtensions.SubstringOptions.Trimmed);
 
-            if (!TryGetStringBetweenChars(ref index, lines, BraceStart, BraceEnd, out var ston))
+            if (!TryGetStringBetweenTwoChars(ref index, lines, BraceStart, BraceEnd, out var ston))
                 return ParsedValue<IStonValue>.Invalid();
 
             var parsed = await DeserializeAsync(ston, ctx);
             return new ParsedValue<IStonValue>(index, key, parsed);
         }
 
-        private static bool TryGetStringBetweenChars(ref int index, string[] lines, char charFrom, char charTo, out string result)
+        private static bool TryGetStringBetweenOneChar(ref int index, string[] lines, char charOne, out string result)
         {
             var line = lines[index];
-            var braceStartIndex = line.IndexOf(charFrom);
-            if (braceStartIndex < 0)
+            var charStartIndex = line.IndexOf(charOne);
+            if (charStartIndex < 0)
             {
                 result = default;
                 return false;
             }
 
-            var intent = 0;
-            var ston = default(string);
+            var started = false;
+            var ended = false;
+            var unescape = false;
             StonCache<StringBuilder>.Pop(out var sb);
             for (int i = index; i < lines.Length; ++i)
             {
@@ -162,34 +165,114 @@ namespace Ston
 
                 line = lines[i];
 
-                var skip = false;
+                if (started)
+                    sb.Append(Environment.NewLine);
+
                 for (int n = 0; n < line.Length; ++n)
                 {
                     var c = line[n];
+                    if (c == charOne)
+                    {
+                        if (n > 0 && line[n - 1] == '\\')
+                        {
+                            sb.Append(c);
+                            unescape |= true;
+                            continue;
+                        }
+
+                        if (!started)
+                        {
+                            started = true;
+                            continue;
+                        }
+                        else if (!ended)
+                        {
+                            ended = true;
+                            continue;
+                        }
+                    }
+
+                    if (ended)
+                        break;
+                    else if (started)
+                        sb.Append(c);
+                }
+
+                if (ended)
+                    break;
+            }
+            var ston = sb.ToString();
+            if (unescape)
+                ston = Regex.Unescape(ston);
+            StonCache<StringBuilder>.Push(sb);
+
+            if (string.IsNullOrWhiteSpace(ston))
+            {
+                result = default;
+                return false;
+            }
+
+            result = ston;
+            return true;
+        }
+
+        private static bool TryGetStringBetweenTwoChars(ref int index, string[] lines, char charFrom, char charTo, out string result)
+        {
+            if (charFrom == charTo)
+                throw new Exception($"please use {nameof(TryGetStringBetweenOneChar)} instead this");
+
+            var line = lines[index];
+            var charStartIndex = line.IndexOf(charFrom);
+            if (charStartIndex < 0)
+            {
+                result = default;
+                return false;
+            }
+
+            var intent = 0;
+            var ston = default(string);
+            var unescape = false;
+            StonCache<StringBuilder>.Pop(out var sb);
+            for (int i = index; i < lines.Length; ++i)
+            {
+                index = i;
+
+                line = lines[i];
+
+                var append = line;
+                for (int n = 0; n < line.Length; ++n)
+                {
+                    if (n > 0 && line[n - 1] == '\\')
+                    {
+                        unescape |= true;
+                        continue;
+                    }
+
+                    var c = line[n];
                     if (c == charFrom)
                     {
-                        skip = intent == 0;
+                        if (intent == 0)
+                            append = line.Substring(n + 1, line.Length - (n + 1), StonExtensions.SubstringOptions.Trimmed);
                         intent++;
                     }
                     else if (c == charTo)
                     {
                         intent--;
-                        skip = intent == 0;
+                        if (intent == 0)
+                            append = line.Substring(0, n, StonExtensions.SubstringOptions.Trimmed);
                     }
                 }
 
-                var append = !skip;
-                if (append)
-                {
-                    if (sb.Length > 0)
-                        sb.Append(Environment.NewLine);
-                    sb.Append(line);
-                }
+                if (sb.Length > 0)
+                    sb.Append(Environment.NewLine);
+                sb.Append(append);
 
                 if (intent != 0)
                     continue;
 
                 ston = sb.ToString();
+                if (unescape)
+                    ston = Regex.Unescape(ston);
                 break;
             }
             StonCache<StringBuilder>.Push(sb);
@@ -222,14 +305,25 @@ namespace Ston
             if (!TryGetConverter(type, ctx.settings, out var converter))
                 throw new Exception($"unsupported type '{type}'");
 
-            var valueIndex = equalIndex + 1;
-            var valueStr = typeAndValue.Substring(valueIndex, typeAndValue.Length - valueIndex);
+            if (TryGetStringBetweenOneChar(ref index, lines, Quote, out var text))
+            {
+                var stonValue = await converter.DeserializeAsync(type, text, ctx);
+                if (stonValue == null)
+                    throw new Exception($"converter {converter} should to return {nameof(StonValue)} for key '{key}' and type '{type}'");
 
-            var stonValue = await converter.DeserializeAsync(type, valueStr, ctx);
-            if (stonValue == null)
-                throw new Exception($"converter {converter} should to return {nameof(StonValue)} for key '{key}' and type '{type}'");
+                return new ParsedValue<IStonValue>(index, key, stonValue);
+            }
+            else
+            {
+                var valueIndex = equalIndex + 1;
+                var valueStr = typeAndValue.Substring(valueIndex, typeAndValue.Length - valueIndex);
 
-            return new ParsedValue<IStonValue>(index, key, stonValue);
+                var stonValue = await converter.DeserializeAsync(type, valueStr, ctx);
+                if (stonValue == null)
+                    throw new Exception($"converter {converter} should to return {nameof(StonValue)} for key '{key}' and type '{type}'");
+
+                return new ParsedValue<IStonValue>(index, key, stonValue);
+            }
         }
 
         private bool TryGetConverter(string type, StonSettings settings, out IStonConverter result)
